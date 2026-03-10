2b9ab5e7a7b9d4cb53e891fbd1658d7bdefaa5f8
Miqueias Rocha <mimimiqueias82@gmail.com>
2026-03-10 20:51:47 -0300

    Criação da classe usuario e criação da função iniciar_treino

--- app.py
+++ app.py
@@ -26,6 +26,17 @@ class Academia():
         out_position_list = [i for i, j in self.porta_halteres.items() if i != j]
         return len(out_position_list) / len(self.porta_halteres)
 
+class Usuario:
+    def __init__(self, tipo_usuario, academia):
+        self.tipo = tipo_usuario # 1 - Normal | 2 - Bagunceiro
+        self.academia = academia
+        self.peso = 0
+
+    def iniciar_treino(self):
+        lista_pesos = self.academia.listar_halteres()
+        halter_choose = random.choice(lista_pesos)
+        picked_halter = self.academia.pegar_halter(halter_choose)
+        return picked_halter
 
 self = Academia()
 
